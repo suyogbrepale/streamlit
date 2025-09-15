b590e78218e85561fc13820e92757ff90befb43b
suyogbrepale <suyogrepale25@gmail.com>
2025-09-15 20:23:56 +0530

    Update streamlit_app.py
    error debug1

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,10 +14,11 @@ st.write(
 name_on_order = st.text_input("Name of Smoothie:")
 st.write("The name on your Smoothie will be:", name_on_order)
 
+cnx = st.connection("snowflake")
+session =cnx.session()
 
-session = get_active_session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
-#st.dataframe(data=my_dataframe, use_container_width=True)
+
 
 
 ingredients_list = st.multiselect(
@@ -53,5 +54,3 @@ if ingredients_list:
 
     
 
-cnx = st.connection("snowflake")
-session =cnx.session()
